23bb76b19702f52d48bd8d4cc8278ed774037c41
Laurent BOUQUET <laurent.bouquet@joliciel.fr>
2018-06-30 10:58:17 +0200

    [Deploy] Add User fixtures load to Heroku #20

--- src/Migrations/Version20180629142824.php
+++ src/Migrations/Version20180629142824.php
@@ -15,8 +15,6 @@ final class Version20180629142824 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('ALTER SEQUENCE tbl_user_id_seq INCREMENT BY 1');
-        $this->addSql('DROP SEQUENCE category_id_seq CASCADE');
         $this->addSql('CREATE SEQUENCE tbl_category_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE tbl_category (id INT NOT NULL, shortname VARCHAR(50) NOT NULL, longname VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
     }
@@ -27,9 +25,7 @@ final class Version20180629142824 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER SEQUENCE tbl_user_id_seq INCREMENT BY 1');
         $this->addSql('DROP SEQUENCE tbl_category_id_seq CASCADE');
-        $this->addSql('CREATE SEQUENCE category_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('DROP TABLE tbl_category');
     }
 }
