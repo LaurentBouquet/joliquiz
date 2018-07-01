8aaad06e98c05d2ec8b31aec4e8bc9bc777a0a1e
Laurent BOUQUET <laurent.bouquet@joliciel.fr>
2018-07-01 20:40:27 +0200

    [Deploy] Add initial sql dump load for Heroku deployment #20

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-export DATABASE_URL;
-echo "Import initial production data dump to joliquiz database : ${DATABASE_URL}";
-psql --file joliquiz-initial-prod-data.dump.sql ${DATABASE_URL};
+export `heroku config -s`
+echo "Import initial production data dump to joliquiz database : $DATABASE_URL";
+psql --file joliquiz-initial-prod-data.dump.sql $DATABASE_URL;
