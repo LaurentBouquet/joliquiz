55702bc06f14ec2066a082e87944e418743116be
Laurent BOUQUET <laurent.bouquet@joliciel.fr>
2018-07-02 09:46:52 +0200

    [Core] Create PHP entity 'Answer' #6

---
+++ src/Entity/Answer.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\Table;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AnswerRepository")
+ * @ORM\Table(name="tbl_answer")
+ */
+class Answer
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $correct;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getCorrect(): ?bool
+    {
+        return $this->correct;
+    }
+
+    public function setCorrect(bool $correct): self
+    {
+        $this->correct = $correct;
+
+        return $this;
+    }
+}
