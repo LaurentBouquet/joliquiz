58210783fe9b6fe90c1032d0478287f7a13f562f
Laurent BOUQUET <laurent.bouquet@joliciel.fr>
2018-07-17 23:03:00 +0200

    [Deploy] Fix sequence users table error

--- joliquiz-initial-prod-users.dump.sql
+++ joliquiz-initial-prod-users.dump.sql
@@ -9,3 +9,5 @@ INSERT INTO public.tbl_user VALUES (1, 'superadmin', 'superadmin@domain.tld', '$
 INSERT INTO public.tbl_user VALUES (3, 'teacher', 'teacher@domain.tld', '$2y$13$ETcERMGM2/RfX5ApgEXlaeutnmpj/17JFDPLWG5nzJ3xciF01uk32', true, 'a:2:{i:0;s:12:"ROLE_TEACHER";i:1;s:9:"ROLE_USER";}', NULL);
 INSERT INTO public.tbl_user VALUES (4, 'user', 'user@domain.tld', '$2y$13$Y.XPv.owBQ73eZ7juzVW.OPTZTMAP67MG9mZUM9BvAfSnJv3X7Qre', true, 'a:1:{i:0;s:9:"ROLE_USER";}', NULL);
 INSERT INTO public.tbl_user VALUES (2, 'admin', 'admin@domain.tld', '$2y$13$qy/peqCnpCTk20VUJBuM9.axawgEeqXHL2o6YVVmKp6FG4z17M5YG', true, 'a:2:{i:1;s:10:"ROLE_ADMIN";i:2;s:9:"ROLE_USER";}', NULL);
+
+SELECT pg_catalog.setval('public.tbl_user_id_seq', 4, true);
