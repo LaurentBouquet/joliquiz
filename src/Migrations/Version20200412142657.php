fed23c9a25fdbfd46c65a30c66071062f9d50ece
Laurent Bouquet <laurent.bouquet@joliciel.fr>
2020-04-12 16:58:58 +0200

    [Install] Recreate Migration files to cleanup next installations

--- src/Migrations/Version20200412142657.php
+++ src/Migrations/Version20200412142657.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200405170953 extends AbstractMigration
+final class Version20200412142657 extends AbstractMigration
 {
     public function getDescription() : string
     {
